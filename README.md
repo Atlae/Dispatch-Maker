300fd29fe520c641304a4fa9af79d4e1ca287bd2
Gary <71896235+Atlae@users.noreply.github.com>
2021-04-28 23:11:11 -0700

    Now only need to run one file instead of three
    Big big big release!

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Dispatch-Maker
-This Dispatch Maker takes an owner report (using Racoda's [owner report](https://github.com/dithpri/RCES/tree/master/owner_report)) and formats it into a BBCode dispatch.
+This Dispatch Maker v.4.0 takes an owner report (using Racoda's [owner report](https://github.com/dithpri/RCES/tree/master/owner_report)) and formats it into a BBCode dispatch.
 
 ## Process
-1. Run `query.py` to create a list of cards to find the owners of.
-    - If the cards you're looking for aren't queryable, you can manually add the card URLs in `query_links.txt`.
+1. The dispatch making process is now new and improved! You only need to run one program instead of three. Run `allinone.py`.
+    - It may yell at you that you may need to download the [sans](https://pypi.org/project/sans/) library (by Darcania/Zephyrkul). 
+        - If so, make sure that you are using a version of Python that is at least 3.6 but at most 3.8 (3.9 not included).
+        - Run `pip install sans` in your command line/terminal.
+
+2. The program will now ask for information, namely your main nation (for the API agent) and the nation you collect from. It will also ask you to enter in a custom query if you're comfortable with that.
+    - If the cards you're looking for aren't queryable, you can manually add the card URLs in `cards.txt`.
     - **Sample queries**
 
     | What I want to search  | My query |
@@ -11,16 +16,7 @@ This Dispatch Maker takes an owner report (using Racoda's [owner report](https:/
     | S2 TEP cards I haven't collected | `region:the_east_pacific&!deck:s2_tep_collector` |
     | Cards with the Delegate badge at auction | `badge:delegate&auction` |
     | GIF flags *(pronounced with a hard G don't @ me)* | `flag:gif` |
-         
-2. Run `owner_report.py`.
-    - It may yell at you that you may need to download the [sans](https://pypi.org/project/sans/) library (by Darcania/Zephyrkul). 
-        - If so, make sure that you are using a version of Python that is at least 3.6 but at most 3.8 (3.9 not included).
-        - Run `pip install sans` in your command line/terminal.
-    - Select [2] to search the cards that were just queried.
-        - [1] or [3] would also search the cards in the decks of the puppets you listed.
     
-3. Run `bbcode.py` to format the output of the owner report.
-    - The owner report returns a .tsv file named with a timestamp, for example `2021-04-20 11-59-59.tsv`.
-    - When running `bbcode.py`, do not enter in the .tsv extension (the code does that for you) so just enter in `2021-04-20 11-59-59`, for example.
-
-Make sure to credit Racoda and myself in your dispatch.
+3. Your new file should be available at `[main nation] [timestamp].tsv`, all formatted with BBCode and ready to go! Simply add this to your dispatch.
+    - Make sure to credit Racoda and myself in your dispatch.
+    - The next feature that will be added is to auto-generate these dispatches by the API. Stay tuned!
---
+++ allinone.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+    sys.stderr.flush()
+
+try:
+    from sans.api import Api
+    from sans.utils import pretty_string
+    from sans.errors import NotFound
+except ImportError:
+    eprint(
+        """You need `sans` to run this script!
+install it by running
+    python3 -m pip install -U sans
+or
+    py -m pip install -U sans
+or from https://pypi.org/project/sans/"""
+    )
+    input("Press enter to continue...")
+    sys.exit(1)
+
+import asyncio
+import datetime
+import re
+from collections import defaultdict
+
+async def ratelimit():
+    while xra := Api.xra:
+        xra = xra - datetime.datetime.now().timestamp()
+        eprint(f"Rate limit reached: sleeping {int(xra)} seconds...")
+        await asyncio.sleep(xra)
+
+async def main():
+    version = 4.0
+    print("Version No. %.1f" % version)
+    username = ""
+    while not username:
+        username = input("Please enter your (main) nation name: ")
+    Api.agent = f"Owner Report (dev. Atlae) (in use by {username})"
+    bidsTrue = False
+    nation = input("What nation are you collecting from? ").lower().replace(" ", "_")
+    query_season = 3
+    while query_season not in [1, 2]:
+        query_season = input("What season are you looking for? (1 or 2) ")
+        try:
+            query_season = int(query_season)
+        except ValueError:
+            print("That's not a number!")
+        except query_season == 3:
+            print("S3 will never come.")
+    custom = input("Do you want to make your own custom query? (yes/no) ").lower().startswith('y')
+    if not custom:
+        region = input("What region are you searching? ").lower().replace(" ", "_")
+        bids = input("Are you looking for the cards to bid on? (yes/no) ")
+        if bids.lower().startswith('y'):
+            bidsTrue = True
+
+    if custom:
+        posted_query = input("Please enter your query using the Advanced Cards Queries syntax: ")
+        processed_query = posted_query.replace(":", "%3A").replace("&", "%26").replace("!", "%21").replace("|", "%7C").replace(" ", "+").replace("(", "%28").replace(")", "%29")
+        query = f'http://azure.nsr3n.info/card_queries/get_daemon_advanced.sh?format=full&query={processed_query}&season={query_season}&format=full&submit=submit'
+    elif bidsTrue:
+        query = f'http://azure.nsr3n.info/card_queries/get_daemon_advanced.sh?format=full&query=region%3A{region}%26%21deck%3A{nation}%26%21bid%3A{nation}&season={query_season}&format=full&submit=submit'
+    else:
+        query = f'http://azure.nsr3n.info/card_queries/get_daemon_advanced.sh?format=full&query=region%3A{region}%26%21deck%3A{nation}&season={query_season}&format=full&submit=submit'
+
+    print('Running...accessing r3n\'s server')
+    reqs = requests.get(query)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    print("Finished accessing r3n\'s server")
+
+    print("Writing the output of said query into file")
+    with open('cards.txt', 'w') as f:
+        a = soup.find_all('a')
+        for i in range(1, len(a)-1):
+            f.write(a[i].get('href') + '\n')
+        f.write(a[len(a)-1].get('href'))
+
+    cards = set()
+    with open("cards.txt", "r") as lines:
+        linenum = 0
+        for line in lines:
+            linenum += 1
+            if temp := re.match(r"^https?://(www\.)?nationstates.net/page=deck/card=(?P<id>[0-9]+)(/season=(?P<season>[0-9]+))?", line):
+                id, season = temp.group("id"), temp.group("season")
+            elif temp := re.match("(?P<id>[0-9]+)(([^0-9])+(?P<season>[0-9]+))?", line):
+                id, season = temp.group("id"), temp.group("season")
+            else:
+                eprint(f"Unable to process line {linenum} because you put in a wrong format")
+                continue
+            if season:
+                cards.add((id, season))
+            else:
+                for s in range(3):
+                    cards.add((id, s))
+
+    file_name = datetime.datetime.now().strftime(f"{username} %Y-%m-%d %H-%M-%S.tsv")
+    output_file = open(file_name, "x")
+    # output_file.write("ID\tSEASON\tNUMBER OF OWNERS\tNUMBER OF COPIES\tOWNERS:COPIES\n")
+    # output_file.write("[table][tr][td][b]CARD LINK[/b][/td][td][b]OWNERS[/b][/td][/tr]\n")
+    output_file.write("[table][tr][td][b]CARD LINK[/b][/td][td][b]NUMBER OF OWNERS[/b][/td][td][b]NUMBER OF COPIES[/b][/td][td][b]OWNERS[/b][/td][/tr]\n")
+    for card in cards:
+        id, season = card
+        owners_dict = defaultdict(int)
+        num_owners = 0
+        num_copies = 0
+        owners_copies = "[i]No owners... :([/i]"
+        await ratelimit()
+        result = await Api("card owners", cardid=id, season=season)
+        try:
+            for owner in result.OWNERS.OWNER:
+                num_copies += 1
+                owners_dict[owner.text] += 1
+        except AttributeError:
+            if result.find("OWNERS") == None:
+                eprint(f"Card {id} season {season} does not exist.")
+                continue
+        owners = owners_dict.keys()
+        num_owners = len(owners)
+        if num_owners > 0:
+            owners_copies = ",".join(
+                [
+                    ":".join((a, str(b)))
+                    for a, b in sorted(
+                        owners_dict.items(), key=lambda x: x[1], reverse=True
+                    )
+                ]
+            )
+            owners_copies = re.sub(r":\d,", "[/nation][*][nation]", owners_copies)
+            owners_copies = re.sub(r":\d", "[/nation]", owners_copies)
+            owners_copies = "[list][*][nation]" + owners_copies + "[/list]"
+        output_file.write(
+            f"[tr][td][url=https://www.nationstates.net/page=deck/card={id}/season={season}]Link to Card[/url][/td][td]{num_owners}[/td][td]{num_copies}[/td][td]{owners_copies}[/td][/tr]\n"
+        )
+        print(f"Added {card}")
+    output_file.write("[tr][td][i]This table was generated with the help of [nation]Racoda[/nation]'s RCES owner report, which can be found [url=https://github.com/dithpri/RCES]here.[/url] I coded a way to automate this [url=https://github.com/Atlae/Dispatch-Maker]here[/url]. -[nation]Atlae[/nation][/i][/td][/tr][/table]")
+
+if __name__ == "__main__":
+    asyncio.run(main())
